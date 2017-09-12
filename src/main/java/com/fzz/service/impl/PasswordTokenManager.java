117a660afa1c143b9d0b85ae5933c3c3efeebf1f
fzz <fzz@ec.com.cn>
2017-09-12 13:37:08 +0800

    'init

---
+++ src/main/java/com/fzz/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.fzz.service.impl;
+
+import com.fzz.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
